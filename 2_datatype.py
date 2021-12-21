55038a789bbce6d0ff2aaa2e503b41b7ecf6ca21
hanumanjani <hanumanaram2120@gmail.com>
2021-12-21 14:44:03 +0530

    datatype

---
+++ 2_datatype.py
@@ -0,0 +1,41 @@
+a_str = "hello world"
+"""
+set data type 
+"""
+basket = {'apple', 'orange', 4, 5}
+print(basket)
+
+# number data type
+
+int_num = 34
+float_num = 457.84
+complex_no = 437j
+long_no = 123456765736576378567236747657534736745246756735673
+print(int_num)
+print(float_num)
+print(complex_no)
+print(long_no)
+
+# list data type
+
+list1 = [1, 2, 3, 'hanumanjani', 'chemistry']
+print(list1)
+print(list1[0:2])
+print('--------------------------------------------------------------')
+
+# dictionary data type
+
+dic1 = {'name' : 'red' , 'age': 10}
+print(dic1)
+print(dic1['name'])
+print(dic1.keys())
+print(dic1.values())
+
+# tuple data type
+
+tuple = (123, 'hello')
+tuple1 = ('world')
+print(tuple)
+# print(tuple + tuple1)
+print(tuple[0])
+# tuple[1] = 'update'        ----->show error
